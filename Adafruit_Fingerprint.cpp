ce02454e4984430b102788c3aa84d279fbf804ab
rfk3987 <rfk3987@gmail.com>
2019-07-04 19:29:55 -0700

    fix typo

--- Adafruit_Fingerprint.cpp
+++ Adafruit_Fingerprint.cpp
@@ -7,7 +7,7 @@
   These displays use TTL Serial to communicate, 2 pins are required to
   interface
   Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-sourceshardware by purchasing
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
   Written by Limor Fried/Ladyada for Adafruit Industries.
